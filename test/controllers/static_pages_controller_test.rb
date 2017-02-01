ab39d7f6ba69cfabde7a7f1aa6ea2b85888bfe4d
Tobi Abawonse <oabawons@mail.depaul.edu>
2017-02-01 23:35:52 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,79 @@
+# require 'test_helper'
+
+# class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+#   test "should get home" do
+#     get static_pages_home_url
+#     assert_response :success
+#   end
+
+#   test "should get help" do
+#     get static_pages_help_url
+#     assert_response :success
+#   end
+#   test "should get about" do
+#     get static_pages_about_url
+#     assert_response :success
+#   end
+# end
+# require 'test_helper'
+
+# class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+#   test "should get home" do
+#     get static_pages_home_url
+#     assert_response :success
+#     assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+#   end
+
+#   test "should get help" do
+#     get static_pages_help_url
+#     assert_response :success
+#     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+#   end
+
+#   test "should get about" do
+#     get static_pages_about_url
+#     assert_response :success
+#     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+#   end
+# end
+
+# require 'test_helper'
+
+# class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+#   def setup
+#     @base_title = "Ruby on Rails Tutorial Sample App"
+#   end
+
+#   test "should get home" do
+#     get static_pages_home_url
+#     assert_response :success
+#     assert_select "title", "Home | #{@base_title}"
+#   end
+
+#   test "should get help" do
+#     get static_pages_help_url
+#     assert_response :success
+#     assert_select "title", "Help | #{@base_title}"
+#   end
+
+#   test "should get about" do
+#     get static_pages_about_url
+#     assert_response :success
+#     assert_select "title", "About | #{@base_title}"
+#   end
+# end
+
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get root" do
+    get FILL_IN
+    assert_response FILL_IN
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -11,4 +84,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
 end
