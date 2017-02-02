1c63a24fa3942a6645ae0490a5ec4b44921e5732
Tobi Abawonse <oabawons@mail.depaul.edu>
2017-02-02 04:59:15 +0000

    The move

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,28 @@
+# # Rails.application.routes.draw do
+#   get 'users/new'
 
+# #   get 'static_pages/home'
+
+# #   get 'static_pages/help'
+
+# #   resources :microposts
+# #   resources :users
+# #   root 'users#index'
+# #   root 'application#hello'
+# end
 Rails.application.routes.draw do
 
-  get  'static_pages/home'
-
+  # get  'static_pages/home'
+  # get  'static_pages/help'
+  # get  'static_pages/about'
+  # get  'static_pages/contact'
+  get 'users/new'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  resources :microposts
+  resources :users
+  # root 'users#index'
   root 'static_pages#home'
 end
