7235077ee52e8b875a70bae5356efb4494fca1a4
Tobi Abawonse <oabawons@mail.depaul.edu>
2017-02-02 01:09:22 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 # Rails.application.routes.draw do
+  get 'users/new'
+
 #   get 'static_pages/home'
 
 #   get 'static_pages/help'
@@ -9,8 +11,15 @@
 #   root 'application#hello'
 # end
 Rails.application.routes.draw do
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
+  get 'users/new'
+
+  # get  'static_pages/home'
+  # get  'static_pages/help'
+  # get  'static_pages/about'
+  # get  'static_pages/contact'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
   root 'static_pages#home'
 end
